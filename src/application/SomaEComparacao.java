4d61cafaae86ffdb24188beee5adec8cb51ae526
Maisa Gomes <maisakichlergms@gmail.com>
2024-04-25 14:58:50 -0300

    exercicio1

---
+++ src/application/SomaEComparacao.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Scanner;
+
+public class SomaEComparacao {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println ("Digite o valor de A:");
+		int a = sc.nextInt();
+		
+		System.out.println ("Digite o valor de B:");
+		int b = sc.nextInt();
+		
+		System.out.println ("Digite o valor de C:");
+		int c = sc.nextInt();
+		
+		int soma = a + b;
+		
+		if (soma < c) {
+			System.out.println("A soma de A e B é menor que C.");
+		} else {
+			System.out.println("A soma de A e B não é menor que C.");
+		}
+		
+	}
+	
+	
+	
+	
+	
+}
